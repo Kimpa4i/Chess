e029e81eb8e63be1bb2d1d19933ebd78e477f730
andrew kim <mr.kimpa4i@gmail.com>
2023-03-25 21:45:45 +0300

    drug&drop move

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const figures = document.querySelectorAll(".figure");
 const gamecells = document.querySelectorAll(".gamecell");
 
-console.log(figures);
-
 figures.forEach(figure => {
   figure.addEventListener("dragstart", dragStart);
 });
@@ -11,6 +9,7 @@ let beingDragged;
 
 function dragStart(e) {
   beingDragged = e.target;
+  console.log(beingDragged);
 }
 
 gamecells.forEach(gamecell => {
@@ -19,9 +18,11 @@ gamecells.forEach(gamecell => {
 });
 
 function dragDrop(e) {
+  e.target.innerText = "";
   e.target.append(beingDragged);
 }
 
 function dragOver(e) {
   e.preventDefault();
+  console.log(e);
 }
